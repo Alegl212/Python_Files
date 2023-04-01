39432922204a79b6ed8d04647a512c06bacf1920
Alegl212 <alegomeez1@gmail.com>
2023-04-01 12:02:54 -0300

    Subiendo archivos de Python

---
+++ Types/02_strings.py
@@ -0,0 +1,13 @@
+course_name = "Ultimate Python"
+course_desc = """
+Ultimate Python, shadow se la come doblada o sea goooood.
+"""
+print(course_name, course_desc)
+
+print(len(course_name))
+
+print(course_name[0])
+
+print(course_name[:8])
+
+print(course_name[9:])
---
+++ Types/03_format-strings.py
@@ -0,0 +1,4 @@
+name = "Alejandro"
+surname = "Gomez"
+full_name = "Hola mi nombre es {} y mi apellido es {}".format(name, surname)
+print(full_name)
